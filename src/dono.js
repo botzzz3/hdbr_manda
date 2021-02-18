f18edb9cfaa5e9ebc9310d09d9fdf1f3f14ba22c
botzzz3 <77111954+botzzz3@users.noreply.github.com>
2021-02-17 23:40:14 -0300

    Add files via upload

---
+++ src/dono.js
@@ -0,0 +1,9 @@
+const dono  = (prefix, sender) => {
+	return `DONO DO BOT :)
+
+CRIADO POR BY CHAROPE
+DIA 17/FEV/2021
+ESPERO QUE GOSTE DO BOT :)`
+}
+
+exports.dono = dono
---
+++ src/maloka.js
@@ -0,0 +1,7 @@
+const maloka = (prefix, sender) => {
+	return `AINDA NAO ESTA PRONTA 
+
+espere ae esta em manutenção 😴`
+}
+
+exports.maloka = maloka
